e0fbcea28bb898550083a197a2f7137ecf6a0a3e
Nouhad122 <nouhadalhallab122@gmail.com>
2025-02-12 15:09:43 +0300

    styling and fetching data using tanstack query

--- src/Components/Home/Exercises.jsx
+++ src/Components/Home/Exercises.jsx
@@ -3,55 +3,73 @@ import classes from './Exercises.module.css'
 import { useLoaderData } from 'react-router';
 
 const Exercises = () => {  
-    const exercises = useLoaderData();
+    // const exercises = useLoaderData();
     const [searchTerms, setSearchTerms] = useState('');  
-    const [searchedExercises, setSearchedExersices] = useState([]);
-    const [loading, setLoading] = useState(true);
+    // const [searchedExercises, setSearchedExersices] = useState([]);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect(() =>{
-      if(searchedExercises){
-        setTimeout(() => setLoading(false), 1000);
-      }
-    },[searchedExercises]);
+    // useEffect(() =>{
+    //   if(searchedExercises){
+    //     setTimeout(() => setLoading(false), 1000);
+    //   }
+    // },[searchedExercises]);
 
     const handleInputChange = (event) =>{
       setSearchTerms(event.target.value);
     }
     
-    const handleSearchResults = () =>{
-      if(searchTerms.trim() !== ''){
-        const filteredExercises = exercises.filter(exercise => JSON.stringify(exercise).includes(searchTerms.toLowerCase()));
-        setSearchedExersices(filteredExercises)
-      } 
-    }
+    // const handleSearchResults = () =>{
+    //   if(searchTerms.trim() !== ''){
+    //     const filteredExercises = exercises.filter(exercise => JSON.stringify(exercise).includes(searchTerms.toLowerCase()));
+    //     setSearchedExersices(filteredExercises)
+    //   } 
+    // }
 
-    if(loading){
-      return <p>Fetching exercises...</p>
-    }
+    // if(loading){
+    //   return <p>Fetching exercises...</p>
+    // }
 
   return (
     <>
       <div className={classes['exercises-search']}>
-        <h1>Search For Exercises</h1>
+        <h1 className={classes['header']}>Search For Exercises</h1>
         <div className={classes['search-input']}>
           <input type='text' id='text' name='text' value={searchTerms} onChange={handleInputChange} />
-          <button onClick={handleSearchResults}>Search</button>
+          <button className={classes['btn']}>Search</button>
         </div>
       </div>
 
       <div className={classes['exercises-list']}>
-        {
-          searchedExercises.map(exercise =>(
-            <div className={classes['exercices-card']} key={exercise.id}>
-              {/* <img src={exercise.gifUrl} alt={exercise.name}/> */}
-              <h5>
-                <span>{exercise.bodyPart}</span>
-                <span>{exercise.target}</span>
+        {/* {
+          searchedExercises.map(exercise =>( */}
+            <div className={classes['exercise-card']}>
+              <img src="https://v2.exercisedb.io/image/EAZ-j7IevIhqKJ" alt=''/>
+              <h5 className={classes['sub-headings']}>
+                <span>waist</span>
+                <span>abs</span>
               </h5>
-              <h3>{exercise.name}</h3>
+              <h3 className={`${classes['exercise-name']} ${classes['header']}`}>exercise name</h3>
             </div>
-          ))
-        }
+
+            <div className={classes['exercise-card']}>
+              <img src="https://v2.exercisedb.io/image/EAZ-j7IevIhqKJ" alt=''/>
+              <h5 className={classes['sub-headings']}>
+                <span>waist</span>
+                <span>abs</span>
+              </h5>
+              <h3 className={`${classes['exercise-name']} ${classes['header']}`}>exercise name</h3>
+            </div>
+
+            <div className={classes['exercise-card']}>
+              <img src="https://v2.exercisedb.io/image/EAZ-j7IevIhqKJ" alt=''/>
+              <h5 className={classes['sub-headings']}>
+                <span>waist</span>
+                <span>abs</span>
+              </h5>
+              <h3 className={`${classes['exercise-name']} ${classes['header']}`}>exercise name</h3>
+            </div>
+          {/* ))
+        } */}
       </div>
     </>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,9 @@ const Home = () => {
   return (
     <>
       <ImageBox />
-      <Exercises />
+      <section>
+        <Exercises />
+      </section>
     </>
   )
 }
